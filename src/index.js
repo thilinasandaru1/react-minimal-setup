b102ed211f33d3807c594070c54679ec81c1628c
Thilina Sandaruwan <tsandaruwan@localhost.localdomain>
2019-02-02 17:50:31 +0530

    minimal-react-setup - initialized setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const title = "My minimal React Webpack Babel Setup";
+
+ReactDOM.render(<div>{title}</div>, document.getElementById("app"));
